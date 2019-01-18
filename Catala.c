7ce914d3e562f8a9144fbb5b822890d95558690f
André <andrevitorino1997@hotmail.com>
2019-01-18 23:24:52 +0000

    Nova perda de dados no Catala.c, Programa sem informação. Eliminação de "Catala.c" e "Catala". Criação de "Programa.c" e cópia de dados para este.

--- Catala.c
+++ Catala.c
@@ -1,24 +1,82 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <graphics.h>
+#include <conio.h>
 
-int main (){
-	FILE *fp;
-	fp = fopen("map2.ini","r");
-	char grid_values[9][9];
-	char save[9];
+int main()
+{
 
-	int i;
-	for (i = 0; (fscanf(fp, "%s", save) != EOF); i++)
-	{
-		int n;
-		for (n = 0; n <=9; n++)
-		{
-			grid_values[i][n] = save[n];
-			printf("%c\n", grid_values[i][n]);
-		}
+	int looper;
+	int diceCounter = 1;
+	int result;
+	int result2;
+	int sum = 0;
+	char response;
+
+	srand(time(NULL)); // to make the randomization independent of the computer's fixed seed
+
+	for (looper = 1; looper <= 2; looper++) {
+
+		printf("Dice #%d:\t%d\n", diceCounter, result = rand() % 6 + 1);
+		sum += result;
+		diceCounter++;
+
 	}
-	fclose(fp);
+
+	printf("Three dices were rolled.\nTheir sum is \t%d\n", sum);
+
+	/*if sum = 9
+	{
+	//PL1 recebe wool, PL2 recebe grain.
+	}*/
+
+	int gd = DETECT, gm;
+	initgraph(&gd, &gm, "C:\\TC\\BGI");
+
+	/* Draw Bar on screen */
+	bar(150, 200, 400, 350);
+
+	getch();
+	closegraph();
+	return 0;
+
+}
+
+
+int drawGrid(void)
+{
+	puts("+--------------+--------------+--------------+--------------+");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("+--------------+--------------+--------------+--------------+");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("+--------------+--------------+--------------+--------------+");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("+--------------+--------------+--------------+--------------+");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("|              |              |              |              |");
+	puts("+--------------+--------------+--------------+--------------+");
 	return 0;
 }
+
